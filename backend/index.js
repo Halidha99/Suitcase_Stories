b0aa7e1dcc6a6a829fc8871d2fc5160380973bcf
Halidha Aarif <96538764+Halidha99@users.noreply.github.com>
2025-09-30 16:42:49 +0530

    setup mongodb

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const bcrypt = require("bcrypt");
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+// Test API
+app.get("/hello", async (req, res) => {
+  return res.status(200).json({ message: "hello" });
+});
+
+// Start server
+app.listen(8000);
+ 
+module.exports = app;

8f79641590b88113465dc7b4a11ca49c77c3f437
Halidha Aarif <96538764+Halidha99@users.noreply.github.com>
2025-10-01 12:15:43 +0530

    add mongoose congigration

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+
 const bcrypt = require("bcrypt");
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
+mongoose.connect(config.connectionString);
 const app = express();
 
 // Middleware
